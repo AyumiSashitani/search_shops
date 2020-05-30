63ed13c97a0f9520ecc4d10523caa953d215944d
sashitani ayumi <sasitani_611@yahoo.co.jp>
2020-05-30 16:42:16 +0900

    初期コミット（遷移先の画面を作成）

---
+++ lib/MapPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MapPage extends StatefulWidget {
+  @override
+  createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
